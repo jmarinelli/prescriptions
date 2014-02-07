90c27f982bdcf3496770d458d917768f5129c80f
Juan Marinelli <juanjosemarinelli@gmail.com>
2014-02-07 11:45:33 -0300

    Otra forma

--- src/main/java/prescriptions/web/controller/AdminController.java
+++ src/main/java/prescriptions/web/controller/AdminController.java
@@ -1,5 +1,8 @@
 package prescriptions.web.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import prescriptions.domain.entity.Prescription;
@@ -56,17 +58,22 @@ public class AdminController {
 	}
 
 	@RequestMapping(value = "dump", method = RequestMethod.GET)
-	@ResponseBody
-	public String dump(@RequestParam String serCarat) {
+	public String dump(@RequestParam String serCarat) throws IOException {
 		List<Prescription> presc = this.prescriptionsRepo.getBySerCarat(serCarat);
+		
+		File file = new File("/home/juanjo/dumps/dump_" + serCarat + ".txt");
+		
+		FileOutputStream fos = new FileOutputStream(file);
 		
 		StringBuffer sBuffer = new StringBuffer();
 
 		for (Prescription p : presc) {
-			
 			sBuffer.append(p.toString()).append('\n');
 		}
 		
-		return sBuffer.toString();
+		fos.write(sBuffer.toString().getBytes());
+		fos.close();
+		
+		return "redirect:";
 	}
 }
